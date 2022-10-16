0c9e7bf289b81e920cb21eb7702d099e275cdde4
David Garcia <114870623+davidgdt@users.noreply.github.com>
2022-10-16 20:30:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+#construye un programa que al recibir tres 
+#numeros enteros diferentes entre si 
+#determine si estan en orden ascendiente
+n1 = int(imput("ingrese el primer numero: "))
+n2 = int(imput("ingrese el segundo numero: "))
+n3 = int(imput("ingrese el tercer numero: "))
+
+if n1 < n2 < n3:
+     print("orden ascendiente")
+else:
+     print("no orden ascendiente")
+
+     print("error");
+     return 0;
+
+     print("/n")
+
+#ejercicio 2
+numeros=list()
+n1=numeros.append(imput("añade un numero:"))
+n2=numeros.append(imput("añade un numero:"))
+n3=numeros.append(imput("añade un numero:"))
+if numeros[0] == 0:
+    print("error")
+if n1 [0] < n2 [1] < n3 [2]:
+  print("orden ascendiente")
+else:
+    print("no orden ascendiente")
+
+    print("/n")
+
+#ejercicio3 
+ contador =0 
+ while True :
+    frase=imput("intreoduce texto:")
+if frase == "a":
+    contador = contador +1
+    if frase == ".":
+        break
+print(f"aparecen{marcador} letras a")
+ print("/n")
+
+ #ejercicio 4 
+ modelos = ["stratocaster", "telecaster","les Paul", "jazzmaster", "SG"]
+ print(modelos[0], len(modelos[0]))
+ print(modelos[1], len(modelos[1]))
+ print(modelos[2], len(modelos[2]))
+ print(modelos[3], len(modelos[3]))
+ print(modelos[4], len(modelos[4]))
+
+ print(f"el elemento con mas caracteres es:",[0])
+ modelos.sort(key=len)
+ print(modelos)
+ modelos.sort(key=len , reverse=true)
+ print(paises)
+
